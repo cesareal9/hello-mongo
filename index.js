3d25272c78f59e31f8ba997165aa09b5ce96da15
cesareal9 <caugustorama@gmail.com>
2024-09-11 13:16:58 -0300

    Revert "oi"

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+// const express = require('express');
+// const app = express();
+// const port = 5000;
 
-const aeroporto = require('./src/routes/aeroporto.js');
-const caneta = require('./src/routes/caneta.js');
-const moto = require('./src/routes/moto.js');
-const refrigerante = require('./src/routes/refrigerante.js');
+// const aeroporto = require('./src/routes/aeroporto.js');
+// const caneta = require('./src/routes/caneta.js');
+// const moto = require('./src/routes/moto.js');
+// const refrigerante = require('./src/routes/refrigerante.js');
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/aeroporto', aeroporto);
-app.use('/caneta', caneta);
-app.use('/moto', moto);
-app.use('/refrigerante', refrigerante);
+// app.use('/aeroporto', aeroporto);
+// app.use('/caneta', caneta);
+// app.use('/moto', moto);
+// app.use('/refrigerante', refrigerante);
 
-app.listen(port, () => console.log('Servidor escutando na porta ' + port));
+// app.listen(port, () => console.log('Servidor escutando na porta ' + port));
 
 
-app.listen(port, () => console.log("Servidor escutando na porta " + port))
+// app.listen(port, () => console.log("Servidor escutando na porta " + port))
