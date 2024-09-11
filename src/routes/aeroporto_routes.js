bdc6fbe8e2f7ccde10dfb55b3697827d8f3dd07c
cesareal9 <caugustorama@gmail.com>
2024-09-10 21:11:19 -0300

    edicao emergencia

--- src/routes/aeroporto_routes.js
+++ src/routes/aeroporto_routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router()
-
+const cep_endereco = require("../cmiddlewares/cep_endereco.js");
 const aeroporto_controller = require("../controllers/caneta_controller")
 
-router.post("/", aeroporto_controller.store)
+router.post("/",cep_endereco ,aeroporto_controller.store)
 router.get("/", aeroporto_controller.index)
 router.get("/:id", aeroporto_controller.show)
-router.get("/:id", aeroporto_controller.update)
-router.get("/:id", aeroporto_controller.destroy)
+router.put("/:id", cep_endereco,aeroporto_controller.update)
+router.delete("/:id", aeroporto_controller.destroy)
 
 module.exports = Router
