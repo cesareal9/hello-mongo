55e97f1bc8dd6c2207f3ac617be653b3c6ee70c0
cesareal9 <caugustorama@gmail.com>
2024-09-11 13:12:40 -0300

    correcao do codeium

--- src/routes/aeroporto.js
+++ src/routes/aeroporto.js
@@ -1,12 +1,23 @@
+// const express = require("express");
+// const router = express.Router()
+// const cep_endereco = require("../middlewares/cep_endereco.js");
+// const aeroporto = require("../controllers/aeroporto.js")
+
+// router.post("/",cep_endereco ,aeroporto.store)
+// router.get("/", aeroporto.index)
+// router.get("/:id", aeroporto.show)
+// router.put("/:id", cep_endereco,aeroporto.update)
+// router.delete("/:id", aeroporto.destroy)
+
+// module.exports = Router()
+
 const express = require("express");
 const router = express.Router()
-const cep_endereco = require("../middlewares/cep_endereco.js");
-const aeroporto = require("../controllers/aeroporto.js")
 
-router.post("/",cep_endereco ,aeroporto.store)
-router.get("/", aeroporto.index)
-router.get("/:id", aeroporto.show)
-router.put("/:id", cep_endereco,aeroporto.update)
-router.delete("/:id", aeroporto.destroy)
+const aeroporto_controller = require("../controllers/aeroporto_controller")
 
-module.exports = Router()
+router.post("/", aeroporto_controller.store)
+router.get("/", aeroporto_controller.index)
+router.get("/:id", aeroporto_controller.show)
+
+module.exports = router
